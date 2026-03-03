b0b16d91c19a7ef0042c208e326961525583f98e
TRVRNB <travis.renberg@gmail.com>
2026-03-03 11:20:10 -0800

    node files

---
+++ node.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include "node.h"
+using namespace std;
+
+Node::Node(Node* n_next, Node* n_child1, Node* n_child2, string n_meta, string n_data){
+  // constructor, mainly for setting private data
+  children[0] = n_next;
+  children[1] = n_child1;
+  children[2] = n_child2;
+  meta = n_meta;
+  data = n_data;
+}
+
+Node::~Node(){
+  // main question i usually have to ask here: what data is it allowed to access?
+  // i think the node owns its child data, but not next
+  // CHANGE THIS LATER IF THE LINKED LIST NEEDS RECURSIVE DELETION
+  // OR THE BINARY TREE NEEDS IT REMOVED
+  delete children[1];
+  delete children[2];
+}
+
+Node* Node::get_child(unsigned short child_index){
+  // get the child requested in argument
+  return children[child_index]; // 1 or 2
+}
+
+string Node::get_child_data(unsigned short child_index){
+  // skip a step and go straight to this child's data!
+  if (children[child_index] == nullptr){
+    return "";
+  }
+  return children[child_index]->get_data();
+}
+
+bool have_grandkids(){
+  // whether or not any children have children
+  for (Node* child : children){
+    // look at all 3 children, check all 3 of their children
+    // if they have any children at all, return true
+    if (child != nullptr){
+      for (int i = 0; i < 3; i++){
+	if (child->get_child(i) != nullptr){
+	  return true;
+	}
+      }
+    }
+  }
+  return false;
+}
+
+void Node::set_child(unsigned short child_index, Node* n_child){
+  // set this child based on arguments
+  children[child_index] = n_child;
+}
+
+string Node::get_data(){
+  // get data from this node
+  return data;
+}
+
+void Node::set_data(string n_data){
+  // set data based on arguments
+  data = n_data;
+}
---
+++ node.h
@@ -0,0 +1,32 @@
+#ifndef SHUNT_NODE
+#define SHUNT_NODE
+#include <iostream>
+#include <string>
+using namespace std;
+
+// node class, both linear and binary tree
+class Node{
+  // this node is (very slightly) wasteful, since it has a left child, right child, and next ptr
+  // it will be used for either a linked list or binary tree
+  // so the functions for the other class will be unused
+  // why not make 2 classes then? it would hurt performance more than it would help memory
+  // and it would be more inconvenient to make :)
+ public:
+  // constructor / destructor
+  Node(Node* n_next, Node* n_child1, Node* n_child2, string n_meta, string n_data);
+  ~Node();
+  // main functions!
+  // children[0] is next, children[1] and children[2] are left/right child
+  Node* get_child(unsigned short child_index); // 1 = child1, 2 = child2, this way i can use variables
+  string get_child_data(unsigned short child_index); // same as above; this might save some lines
+  void set_child(unsigned short child_index, Node* n_child);
+  string get_data();
+  void set_data(string n_data);
+  string meta; // metadata, aka comments, for debugging purposes
+ private:
+  Node* children[3]{nullptr}; // create 3 empty pointers to nodes (filled in later)
+  string data;
+};
+
+
+#endif
